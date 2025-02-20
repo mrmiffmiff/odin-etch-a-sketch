4f19e8af0afde38f5c0e679471041a41db14836b
Robert Eisenman <robert.t.eisenman@gmail.com>
2025-02-20 15:09:52 -0800

    switch to constant canvas size for now and add squares

--- script.js
+++ script.js
@@ -1,1 +1,26 @@
 const canvas = document.querySelector(".canvas");
+const canvasStyles = window.getComputedStyle(canvas);
+let canvasWidth = parseInt(canvasStyles.getPropertyValue("width"));
+let canvasHeight = parseInt(canvasStyles.getPropertyValue("height"));
+
+let gridSize = 16;
+
+function generatePseudoGrid() {
+    const boxWidth = canvasWidth / gridSize;
+    const boxHeight = canvasHeight / gridSize;
+    console.log(boxWidth);
+    console.log(boxHeight);
+
+    for (let i = 0; i < gridSize; i++) {
+        for (let j = 0; j < gridSize; j++) {
+            const square = document.createElement('div');
+
+            square.style.width = `${boxWidth}px`;
+            square.style.height = `${boxHeight}px`;
+
+            canvas.appendChild(square);
+        }
+    }
+}
+
+generatePseudoGrid();
