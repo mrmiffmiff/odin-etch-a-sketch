9d45e25f4588155588c3bbd56f0d8b86f8062b59
Robert Eisenman <robert.t.eisenman@gmail.com>
2025-02-20 15:31:44 -0800

    function to clear grid

--- script.js
+++ script.js
@@ -6,8 +6,6 @@ let canvasHeight = parseInt(canvasStyles.getPropertyValue("height"));
 function generatePseudoGrid(gridSize = 16) {
     const boxWidth = canvasWidth / gridSize;
     const boxHeight = canvasHeight / gridSize;
-    console.log(boxWidth);
-    console.log(boxHeight);
 
     for (let i = 0; i < gridSize; i++) {
         for (let j = 0; j < gridSize; j++) {
@@ -15,10 +13,18 @@ function generatePseudoGrid(gridSize = 16) {
 
             square.style.width = `${boxWidth}px`;
             square.style.height = `${boxHeight}px`;
+            square.setAttribute("class", "gridSquare");
 
             canvas.appendChild(square);
         }
     }
+}
+
+function clearPseudoGrid() {
+    const squares = document.querySelectorAll(".gridSquare");
+    squares.forEach((currentValue) => {
+        canvas.removeChild(currentValue);
+    });
 }
 
 generatePseudoGrid();
